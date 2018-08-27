a6975900b591d031b9e78e68afe3001e16d5fd2c
hjpark <hjpark@fltgraph.com>
2018-08-27 09:20:18 +0900

    First commit

---
+++ src/main/java/com/hjpark/dockerEx/DockerExApplication.java
@@ -0,0 +1,12 @@
+package com.hjpark.dockerEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerExApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hjpark/dockerEx/HomeController.java
@@ -0,0 +1,16 @@
+package com.hjpark.dockerEx;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+  
+  @RequestMapping(value = "/")
+  public ModelAndView main() {
+      ModelAndView view = new ModelAndView("index");
+      view.addObject("text", "world");
+      return view;
+  }
+}
