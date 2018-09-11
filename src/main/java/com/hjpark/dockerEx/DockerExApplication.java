5f18ba0a7d47604f850ad0ab84d8aa8d6d25d5be
hjpark <hjpark@fltgraph.com>
2018-09-11 12:04:19 +0900

    application java 수정

--- src/main/java/com/hjpark/dockerEx/DockerExApplication.java
+++ src/main/java/com/hjpark/dockerEx/DockerExApplication.java
@@ -1,12 +1,28 @@
 package com.hjpark.dockerEx;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@SpringBootApplication
+@ComponentScan
+@Configuration
+@EnableAutoConfiguration
 public class DockerExApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerExApplication.class, args);
 	}
+	
+	@Bean
+    public InternalResourceViewResolver setupViewResolver() {
+ 
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+ 
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
 }
