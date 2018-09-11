5607889c8ec34936e767099ef2a6e1a6a935f02e
hjpark <hjpark@fltgraph.com>
2018-09-11 15:08:48 +0900

    vue js 로 변경

--- src/main/java/com/hjpark/dockerEx/HomeController.java
+++ src/main/java/com/hjpark/dockerEx/HomeController.java
@@ -2,15 +2,12 @@ package com.hjpark.dockerEx;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
   
-  @RequestMapping(value = "/index")
-  public ModelAndView main() {
-      ModelAndView view = new ModelAndView("index");
-      view.addObject("text", "world");
-      return view;
+  @RequestMapping(value = "/")
+  public String main() {
+      return "index";
   }
 }
