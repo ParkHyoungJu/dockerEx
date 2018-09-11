6b5ebc6743dff8d76d353938baf064b61b627900
hjpark <hjpark@fltgraph.com>
2018-09-11 17:09:45 +0900

    docker compose 생성

--- src/main/java/com/hjpark/dockerEx/DockerExApplication.java
+++ src/main/java/com/hjpark/dockerEx/DockerExApplication.java
@@ -1,28 +1,12 @@
 package com.hjpark.dockerEx;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@ComponentScan
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 public class DockerExApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerExApplication.class, args);
 	}
-	
-	@Bean
-    public InternalResourceViewResolver setupViewResolver() {
- 
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
- 
-        resolver.setPrefix("/WEB-INF/jsp/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
 }
